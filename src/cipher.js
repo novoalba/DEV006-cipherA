f37e19daa5104d1f55bb42842b6f3a09c26e4398
4lb-ita <honnneygi@gmail.com>
2023-03-14 07:31:33 -0600

    primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,20 @@
+//const range = document.getElementById("range").value;
+//const mssg = document.getElementById("mssg").value;
+
 const cipher = {
-  // ...
+  encode: (range, mssg) => {
+    for (var i= 0; i<(mssg.length); i++) {
+      //Posición ascii
+      const asciiPos = mssg.charCodeAt(i);
+      //console.log("asciiPos");
+      //Aplicar fórmula
+      const ciphMssg = (asciiPos - 65 + range) % 26 + 26;
+
+      //Transformar ascii a alfabeto
+      const result = String.fromCharCode(ciphMssg);
+    }
+   return result;
+  }
 };
 
 export default cipher;

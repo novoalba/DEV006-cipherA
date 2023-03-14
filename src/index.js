60678ec2fcca47ba7f50e6d72a0c19e7af4a01b9
4lb-ita <honnneygi@gmail.com>
2023-03-14 07:32:21 -0600

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+/*const offset = document.getElementById("range");
+const txt = document.getElementById("mssg");
+const ciphBtn = document.getElementById("cyphBtn");
+const deciphBtn = document.getElementById("decyphBtn");
+const result = document.getElementById("result");*/
+
+
+const range = document.getElementById("range").value;
+const mssg = document.getElementById("mssg").value;
+

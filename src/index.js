e0fb7d7d923d8f8226562a889c592bfb1382c76a
Alba N <127053877+novoalba@users.noreply.github.com>
2023-03-16 12:14:22 -0600

    Update index.js
    manual commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//declarar variables en index.js:
+const offset = document.getElementById("range")//.value;
+const string = document.getElementById("mssg")//.value//;
+const result = document.getElementById("result");
 
-/*const offset = document.getElementById("range");
-const txt = document.getElementById("mssg");
-const ciphBtn = document.getElementById("cyphBtn");
-const deciphBtn = document.getElementById("decyphBtn");
-const result = document.getElementById("result");*/
+//en el index.js declaramos todo lo que vaya ligado al html:
+const buttonE = document.getElementById("cyphBtn")
+buttonE.addEventListener("click", ()=>{
+  console.log(offset.value, string.value);
+  result.innerHTML = cipher.encode(offset,string);
+});
 
+const buttonD = document.getElementById("decyphBtn")
+buttonD.addEventListener("click", ()=>{
+  
+  result.innerHTML = cipher.decode(offset,string);
+});
 
-const range = document.getElementById("range").value;
-const mssg = document.getElementById("mssg").value;
+const buttonGB = document.getElementById("goBack")
+buttonGB.addEventListener("click",()=>{
+  history.go(0);
+});
+
+//necesitamos aclarar función y variables usadas:
+cipher.encode(offset, string);
+cipher.decode(offset, string);
+
 

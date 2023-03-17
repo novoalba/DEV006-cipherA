9c8781635beea6c9f945333c919b107d52237e5b
4lb-ita <honnneygi@gmail.com>
2023-03-17 11:23:10 -0600

    commit (semi)final

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,54 @@
-//const range = document.getElementById("range").value;
-//const mssg = document.getElementById("mssg").value;
 
+//objeto:
 const cipher = {
-  encode: (range, mssg) => {
-    for (var i= 0; i<(mssg.length); i++) {
-      //Posición ascii
-      const asciiPos = mssg.charCodeAt(i);
-      //console.log("asciiPos");
-      //Aplicar fórmula
-      const ciphMssg = (asciiPos - 65 + range) % 26 + 26;
+  //función encriptar
+  encode: (offset, string) => {
+    //declarar variables para guardar los valores del index
+    const offsetV = offset;
+    const stringV = string;
+    console.log(offsetV);
+    console.log(stringV);
+    let result = "";
+    for (let i= 0; i < stringV.length; i++) {
+      //posición ascii
+      let asciiPos = stringV.charCodeAt(i);
+      console.log(asciiPos);
+      
+      if (asciiPos >= 65 && asciiPos <=90){
+        asciiPos = ((asciiPos - 65 + parseInt(offsetV))%26)+65;
 
-      //Transformar ascii a alfabeto
-      const result = String.fromCharCode(ciphMssg);
-    }
-   return result;
-  }
-};
+      } else if (asciiPos >= 97 && asciiPos <= 122){
+        asciiPos = ((asciiPos - 97 + parseInt(offsetV))%26)+97;
+      }
+      result += String.fromCharCode(asciiPos);
+      console.log(result);
+    } return result;
+  },
+  //función desencriptar
+  decode: (offset, string) => {
+    const offsetV = offset;
+    const stringV = string;
+    console.log(stringV);
+    console.log(offsetV);
+    let result = "";
+    
+    for (let i= 0; i < (stringV.length); i++) {
+      //Posición ascii
+      const asciiPos = stringV.charCodeAt(i);
+      
+      if (asciiPos >= 65 && asciiPos <=90){
+        result += String.fromCharCode((((asciiPos - 65 - parseInt(offsetV) + 26)+26)%26)+65);
+        //asciiPos = (((asciiPos - 65 - parseInt(offsetV) + 26)+26)%26)+65;
+
+      } else if (asciiPos >= 97 && asciiPos <= 122){
+        //asciiPos = ((((asciiPos - 97 - parseInt(offsetV)) + 26)+26)%26)+97;
+        result += String.fromCharCode(((((asciiPos - 97 - parseInt(offsetV)) + 26)+26)%26)+97);
+      }
+      //result += String.fromCharCode(((((asciiPos - 97 - parseInt(offsetV)) + 26)+26)%26)+97);
+      
+    } return result;
+  } 
+}
+
 
 export default cipher;
